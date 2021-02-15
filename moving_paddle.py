a632c10cb966691c0384211b1af2e3151171927f
sn-007 <swamynaidu.ch@research.iiit.ac.in>
2021-02-15 09:43:02 +0530

    paddle started moving with a and d keys from the code in moviing_paddle.py for the first time

---
+++ moving_paddle.py
@@ -0,0 +1,36 @@
+from paddle import *
+from input import *
+from grid import *
+
+b1=Board(max_row,max_col)
+b1.create_board()
+
+paddle=Paddle()
+b1.grid=paddle.intialrender(b1.grid)
+
+b1.print_board()
+
+while(True):
+    getch=Get()
+    key=input_to(getch)
+    if(key=="None"):
+        continue
+    
+    if(key == 'a'):
+        b1.grid=paddle.move_paddle_left(b1.grid)
+        b1.print_board()
+    
+    elif(key == 'd'):
+        b1.grid=paddle.move_paddle_right(b1.grid)
+        b1.print_board()
+    elif(key=="."):
+        break
+
+    
+    
+    
+        
+    
+
+
+
--- paddle.py
+++ paddle.py
@@ -9,26 +9,39 @@ class Paddle:
         self.length=10
         self.xrow=28
         self.ycol=90
+        self.movingflag="0"
+        
 
-    
+    def intialrender(self,grid):
+        for i in range(self.length):
+            grid[self.xrow][self.ycol+i] = Back.BLACK+" "+Style.RESET_ALL
+        return grid
+
     def move_paddle_left(self,grid):
         if(self.ycol > 52):
             self.ycol-=2
-        for i in range(self.length):
-            grid[self.xrow][self.ycol+i] = Back.BLACK+" "+Style.RESET_ALL
-        grid[self.xrow][self.ycol+self.length-1]=" "
-        grid[self.xrow][self.ycol+self.length-2]=" "
+            for j in range(max_col):
+                grid[self.xrow][j]=" "
+            for i in range(self.length):
+                grid[self.xrow][self.ycol+i] = Back.BLACK+" "+Style.RESET_ALL
+            # if(self.movingflag=="L"):
+            #     grid[self.xrow][self.ycol+self.length-1]=" "
+            #     grid[self.xrow][self.ycol+self.length-2]=" "
+            # self.movingflag="L"
     
         return grid
     
     def move_paddle_right(self,grid):
         if(self.ycol + 9 < 141):
             self.ycol+=2
-        for i in range(self.length):
-            grid[self.xrow][self.ycol+i] = Back.BLACK+" "+Style.RESET_ALL
-        grid[self.xrow][self.ycol]=" "
-        grid[self.xrow][self.ycol+1]=" "
-    
+            for j in range(max_col):
+                grid[self.xrow][j]=" "
+            for i in range(self.length):
+                grid[self.xrow][self.ycol+i] = Back.BLACK+" "+Style.RESET_ALL
+            # if(self.movingflag=="R")
+            #     grid[self.xrow][self.ycol]=" "
+            #     grid[self.xrow][self.ycol+1]=" "
+               
         return grid
     
         
@@ -37,29 +50,29 @@ class Paddle:
 
 
 
-#testing bro
+# #testing bro
 
 
-#intiialising grid
-b1=Board(max_row,max_col)
-b1.create_board()
+# #intiialising grid
+# b1=Board(max_row,max_col)
+# b1.create_board()
 
-#creating paddle
-paddle= Paddle()
+# #creating paddle
+# paddle= Paddle()
+# #b1.grid=paddle.intialrender(b1.grid)
 
 
-for i in range(25):
-    if(True):
-        #moving paddle
-        b1.grid =paddle.move_paddle_right(b1.grid)
+# for i in range(100):
 
-        #prinitng background
-        b1.print_board()
-    # else:
-    #     paddle.move_paddle_right()
-    #     b1.grid= paddle.render_paddle(b1.grid)
-    #     b1.print_board()
-    time.sleep(0.13)
+#     if(i%3==1):
+#         b1.grid =paddle.move_paddle_left(b1.grid)
+#         b1.print_board()
+
+#     elif(i%3==2):
+#         b1.grid =paddle.move_paddle_right(b1.grid)
+#         b1.print_board()
+       
+#     time.sleep(0.1)
         
 
 
